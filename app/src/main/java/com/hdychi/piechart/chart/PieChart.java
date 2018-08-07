29ec3f4f5d61d041c9dd7cc3f64d0621a31234ba
黄丹阳 <huangdanyang@meituan.com>
2018-08-07 12:53:30 +0800

    添加饼图旋转功能

--- app/src/main/java/com/hdychi/piechart/chart/PieChart.java
+++ app/src/main/java/com/hdychi/piechart/chart/PieChart.java
@@ -5,8 +5,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.text.DecimalFormat;
@@ -19,9 +21,10 @@ public class PieChart extends View {
     private static final int DEFAULT_WIDTH = 800;
     private static final int DEFAULT_HEIGHT = 800;
     private static final int DEFAULT_LINE_OFFSET = 10;
+    private static final int TOUCH_SLOT = 50;
 
     private List<PieChartItem> pieChartItems;
-    private static float mItemTextSize= 50.0f;
+    private static float mItemTextSize = 50.0f;
     private Paint mPaintForMsg;
     private Paint mPaintForArc;
     private Paint mPaintForName;
@@ -33,9 +36,9 @@ public class PieChart extends View {
 
     //红，蓝，黄，绿 ，灰，橙，紫
     private int[] defaultColors = {Color.parseColor("#FF4500"),
-            Color.parseColor("#4876FF"),Color.parseColor("#FFEC8B"),
-            Color.GREEN,Color.parseColor("#B3B3B3"),
-            Color.parseColor("#EE7600"),Color.parseColor("#BF3EFF")};
+            Color.parseColor("#4876FF"), Color.parseColor("#FFEC8B"),
+            Color.GREEN, Color.parseColor("#B3B3B3"),
+            Color.parseColor("#EE7600"), Color.parseColor("#BF3EFF")};
 
     public PieChart(Context context) {
         super(context);
@@ -86,6 +89,7 @@ public class PieChart extends View {
         mPaintForLine.setStrokeWidth(5);
         mPaintForLine.setAntiAlias(true);
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -108,7 +112,7 @@ public class PieChart extends View {
             totalCnt += item.getCount();
         }
 
-        float nowAngle = 0.0f;
+        float nowAngle = angle;
         for (int i = 0; i < pieChartItems.size(); i++) {
             PieChartItem pieChartItem = pieChartItems.get(i);
             mPaintForArc.setColor(defaultColors[i % defaultColors.length]);
@@ -118,7 +122,7 @@ public class PieChart extends View {
             canvas.drawArc(left, top, right, bottom, nowAngle, -sweepAngle, true,
                     mPaintForArc);
             //画出数据标明线
-            float lineLen = radius + Math.min(radius / 5 , radius / 5);
+            float lineLen = radius + Math.min(radius / 5, radius / 5);
             float startX = (float) (centerX
                     + radius * 0.75 * Math.cos(Math.PI * (-nowAngle + sweepAngle / 2) / 180));
             float startY = (float) (centerY
@@ -128,17 +132,16 @@ public class PieChart extends View {
             float endY = (float) (centerY
                     - lineLen * Math.sin(Math.PI * (-nowAngle + sweepAngle / 2) / 180));
             path.reset();
-            path.moveTo(startX,startY);
-            path.lineTo(endX,endY);
-            endX = endX + (endX < centerX ? -(lineLen - radius) / 2:(lineLen - radius) / 2);
-            path.lineTo(endX,endY);
-            canvas.drawPath(path,mPaintForLine);
+            path.moveTo(startX, startY);
+            path.lineTo(endX, endY);
+            endX = endX + (endX < centerX ? -(lineLen - radius) / 2 : (lineLen - radius) / 2);
+            path.lineTo(endX, endY);
+            canvas.drawPath(path, mPaintForLine);
             //根据扇形的位置决定信息文字的左右对齐
             if (endX < centerX) {
                 mPaintForName.setTextAlign(Paint.Align.RIGHT);
                 mPaintForCount.setTextAlign(Paint.Align.RIGHT);
-            }
-            else{
+            } else {
                 mPaintForName.setTextAlign(Paint.Align.LEFT);
                 mPaintForCount.setTextAlign(Paint.Align.LEFT);
             }
@@ -148,28 +151,73 @@ public class PieChart extends View {
             float maxTextWidth = Math.max(mPaintForName.measureText(name),
                     mPaintForCount.measureText(count));
             while (endX + maxTextWidth > getWidth() || endX - maxTextWidth < 0) {
-                mItemTextSize =  mItemTextSize * 0.9f;
+                mItemTextSize = mItemTextSize * 0.9f;
                 mPaintForName.setTextSize(mItemTextSize);
                 mPaintForCount.setTextSize(mItemTextSize);
                 maxTextWidth = Math.max(mPaintForName.measureText(name),
                         mPaintForCount.measureText(count));
             }
             //绘制当前项的信息
-            canvas.drawText(name,endX,endY, mPaintForName);
-            canvas.drawText(count, endX,endY + mItemTextSize + DEFAULT_LINE_OFFSET,
+            canvas.drawText(name, endX, endY, mPaintForName);
+            canvas.drawText(count, endX, endY + mItemTextSize + DEFAULT_LINE_OFFSET,
                     mPaintForCount);
             nowAngle -= sweepAngle;
         }
     }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        int oldWidth = Math.max(DEFAULT_WIDTH,getWidth());
-        int oldHeight = Math.max(DEFAULT_HEIGHT,getHeight());
+        int oldWidth = Math.max(DEFAULT_WIDTH, getWidth());
+        int oldHeight = Math.max(DEFAULT_HEIGHT, getHeight());
 
-        int newWidth = resolveSize(oldWidth,widthMeasureSpec);
-        int newHeight = resolveSize(oldHeight,heightMeasureSpec);
-        setMeasuredDimension(newWidth,newHeight);
+        int newWidth = resolveSize(oldWidth, widthMeasureSpec);
+        int newHeight = resolveSize(oldHeight, heightMeasureSpec);
+        setMeasuredDimension(newWidth, newHeight);
+    }
+
+    boolean isDragging = false;
+    float lastX, lastY;
+    float firstX, firstY;
+    float angle = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (pieChartItems.size() == 0) {
+            return false;
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                firstX = event.getX();
+                firstY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!isDragging) {
+                    lastX = event.getX();
+                    lastY = event.getY();
+                    if (Math.abs(lastX - firstX) + Math.abs(lastY - firstY) > TOUCH_SLOT) {
+                        isDragging = true;
+                    }
+                }
+                if (isDragging) {
+                    int centerX = getWidth() / 2;
+                    int centerY = getHeight() / 2;
+                    float nowX = event.getX() - centerX;
+                    float nowY = event.getY() - centerY;
+                    angle += (Math.atan2(nowY, nowX) - Math.atan2(lastY - centerY, lastX - centerX))
+                            * 180 /Math.PI;
+                    lastX = event.getX();
+                    lastY = event.getY();
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                isDragging = false;
+                break;
+
+        }
+        return true;
     }
 
     public void addItem(PieChartItem item) {
